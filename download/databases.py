3bbc70307fd3a5a6c4169eecdbb89561ad5f8c4c
sulmanm1 <130704650+sulmanm1@users.noreply.github.com>
2025-01-14 19:38:36 -0500

    Delete download directory

--- download/databases.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-def database_info():
-    classes_data = [
-        ["Caudoviricetes", "https://drive.google.com/drive/folders/1n8c7zDcGxfq8HjueIt2rt-GfDrLCbtya?usp=drive_link", "unknown_processed"]
-    ]
-    classes_df = pd.DataFrame(classes_data, columns=["Class", "Unprocessed_url", "Processed_url"])
-    return classes_df
