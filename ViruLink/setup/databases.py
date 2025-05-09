1173a03f92c6f23a9e8a4fc9a72e3c9c154b1659
sulmanm1 <130704650+sulmanm1@users.noreply.github.com>
2025-05-08 22:26:36 -0400

    Delete ViruLink directory

--- ViruLink/setup/databases.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-def database_info():
-    classes_data = [
-        ["Caudoviricetes", "https://drive.google.com/drive/folders/1n8c7zDcGxfq8HjueIt2rt-GfDrLCbtya?usp=drive_link", "unknown_processed", "--medium", "--medium"],
-        ["Herviviricetes", "https://drive.google.com/drive/folders/1TI5qJ7Gsdp0ksqAUlD_kAsbdCe4LtsWk?usp=drive_link", "unknown_processed", "--medium", "--medium"],
-        ["Monjiviricetes", "https://drive.google.com/drive/folders/1EwIf_UGogrN9zAKJbJbaYEJK4RFTP_Rf?usp=drive_link", "unknown_processed", "--medium", "--small-genomes"]
-    ]
-    classes_df = pd.DataFrame(classes_data, columns=["Class", "Unprocessed_url", "Processed_url", "skani_sketch_mode", "skani_dist_mode"])
-    return classes_df
